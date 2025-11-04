cfeac709bdae7839eabff9f0b7f7e0ddad23630f
Ewan Wallace <ewan.wallace@roche.com>
2025-11-04 18:07:41 +0000

    work in progress

--- src/neural_optimiser/calculators/_fairchem.py
+++ src/neural_optimiser/calculators/_fairchem.py
@@ -1,26 +1,37 @@
+from typing import Literal
+
 import torch
-from fairchem.core.units.mlip_unit import load_predict_unit
 from torch_geometric.data import Batch, Data
 
 from neural_optimiser.calculators.base import Calculator
 
+try:
+    if hasattr(torch, "serialization"):
+        torch.serialization.add_safe_globals([slice])
+except Exception:
+    pass
+
 
 class FAIRChemCalculator(Calculator):
     def __init__(
-        self, model_paths: str, device: str = "cpu", radius: float = 6.0, max_neighbours: int = 32
+        self,
+        model_paths: str,
+        device: str = "cpu",
+        task_name: str = "omol",
+        default_dtype: Literal["float32", "float64"] = "float32",
     ):
         try:
             import fairchem  # noqa: F401
-            from fairchem.core.units.mlip_unit import load_predict_unit  # noqa: F401
+            from fairchem.core.units.mlip_unit import load_predict_unit
         except ImportError:
             raise ImportError(
                 "MACE is not installed. Run `uv pip install fairchem-core` to install."
             )
         self.device = device
-        self.radius = radius
-        self.max_neighbours = max_neighbours
+        self.task_name = task_name
+        self.default_dtype = torch.float32 if default_dtype == "float32" else torch.float64
         self.model_paths = model_paths
-        # self.predictor = load_predict_unit(path=model_paths, device=device)
+        self.predictor = load_predict_unit(path=model_paths, device=device)
 
     def __repr__(self):
         return (
@@ -30,48 +41,22 @@ class FAIRChemCalculator(Calculator):
 
     def _calculate(self, batch: Data | Batch) -> tuple[torch.Tensor, torch.Tensor]:
         """Compute energies and forces for a batch of conformers using a FAIRChem model."""
-        raise NotImplementedError("FAIRChemCalculator is not yet implemented.")
+        from fairchem.core.datasets.atomic_data import AtomicData, atomicdata_list_to_batch
+
+        atoms = batch.to_ase()
+        [
+            atoms[i].info.update({"charge": batch.charge[i], "spin": batch.spin[i]})
+            for i in range(batch.n_conformers)
+        ]
+        atomic_data = [AtomicData.from_ase(atom, task_name="omol") for atom in atoms]
+
+        # TODO: set charge and spin
+
+        batch = atomicdata_list_to_batch(atomic_data)
+        output = self.predictor.predict(batch)
+        return output["energy"], output["forces"]
 
     def get_energies(self, batch: Data | Batch) -> torch.Tensor:
-        raise NotImplementedError("FAIRChemCalculator is not yet implemented.")
-
-    def to_atomic_data(self, batch: Data | Batch) -> Batch:
-        raise NotImplementedError("FAIRChemCalculator is not yet implemented.")
-
-
-if __name__ == "__main__":
-    from ase.build import molecule
-    from fairchem.core import FAIRChemCalculator as fair_calc
-    from fairchem.core import pretrained_mlip
-
-    try:
-        import torch
-
-        if hasattr(torch, "serialization"):
-            torch.serialization.add_safe_globals([slice])
-    except Exception:
-        pass
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model_paths = "./models/omol25_esen_sm_direct.pt"
-
-    # EXAMPLE 1
-    predictor = load_predict_unit(path=model_paths, device=device)
-    calculator = fair_calc(predictor)
-
-    # Create ASE molecule and assign FAIRChem calculator
-    atoms = molecule("H2O")
-    atoms.info = {"charge": 0, "spin": 1}
-    atoms.calc = calculator
-    print(atoms.get_potential_energy())
-
-    # EXAMPLE 2
-    predictor = pretrained_mlip.get_predict_unit("uma-s-1p1", device=device)
-    # get_predict_unit looks up ckpt and calls load_predict_unit.
-    calculator2 = fair_calc(predictor, task_name="omol")
-
-    # Create ASE molecule and assign FAIRChem calculator
-    atoms = molecule("H2O")
-    atoms.info = {"charge": 0, "spin": 1}
-    atoms.calc = calculator2
-    print(atoms.get_potential_energy())
+        """Compute energies for a batch of conformers using a FAIRChem model."""
+        energies, _ = self._calculate(batch)
+        return energies
